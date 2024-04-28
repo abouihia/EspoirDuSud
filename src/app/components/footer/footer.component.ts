378e391cafd54e5b5308f468e6b8a2142608bb4f
brahim abouihia <abouihia.brahim@gmail.com>
2024-04-28 18:42:49 +0200

    add partie s'inscrire et non de exacte association

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder  } from '@angular/forms';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrl: '../../../assets/css/style.css'
 })
 export class FooterComponent {
+
+  email: string='';
 
 }
